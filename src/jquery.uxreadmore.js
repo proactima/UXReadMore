81232bbf425dfaf5863634fa769f0373351266e8
Anders Østhus <grapz666@gmail.com>
2013-09-29 23:23:14 +0200

    Initial commit with basic stuff working

---
+++ src/jquery.uxreadmore.js
@@ -0,0 +1,69 @@
+﻿(function ($, window, undefined) {
+    $.uxreadmore = {
+        version: '1.0.0',
+        defaults: {
+            debug: false,
+            ellipsisType: 'absolute',
+            ellipsisPoint: 200,
+            ellipsisSymbol: '...'
+        }
+    };
+
+    var settings;
+    var debugLog = false;
+
+    $.fn.uxreadmore = function (options) {
+        var target;
+
+        settings = $.extend({}, $.uxreadmore.defaults, options);
+        if (settings.debug === true) {
+            debugLog = true;
+        }
+
+        // FLOW
+        // See if text is longer than cutoffpoint
+        // Determine type:
+        //  - Absolute
+        //  - Nearest word boundary
+        // Cut text
+        // Hide overflow in <span>
+        // Add read more link to original container
+
+        target = $(this);
+
+        if ($(target).text().length >= settings.ellipsisPoint) {
+            if (settings.ellipsisType === 'absolute') {
+                setTargetAttributes(target);
+                extractOverflowText(target);
+            }
+        } else {
+            setTargetAttributes(target, true);
+        }
+    }
+
+    function setTargetAttributes(target, noCut) {
+        if (noCut === true) {
+            $(target).data('cutOff', "noCut");
+        } else {
+            $(target).data('cutOff', settings.ellipsisPoint);
+        }
+    }
+
+    function extractOverflowText(target) {
+        var contents, hiddenSpan;
+
+        contents = $(target).text();
+        hiddenSpan = $('<span></span>');
+        $(target).text(contents.substring(0, settings.ellipsisPoint - 3) + settings.ellipsisSymbol);
+        hiddenSpan.text(contents.substring(settings.ellipsisPoint - 3));
+        hiddenSpan.addClass('hiddenText');
+
+        $(target).append(hiddenSpan);
+    }
+
+    function debug(logItem) {
+        if (debugLog === true) {
+            console.log(logItem);
+        }
+    }
+})(jQuery, window);
---
+++ tests/jquery.uxreadmore.tests.js
@@ -0,0 +1,49 @@
+﻿var targetId = "#qunit-fixture";
+var lorem = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut odio. Nam sed est. Nam a risus et est iaculis adipiscing. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer ut justo. In tincidunt viverra nisl. Donec dictum malesuada magna. Curabitur id nibh auctor tellus adipiscing pharetra. Fusce vel justo non orci semper feugiat. Cras eu leo at purus ultrices tristique.";
+
+QUnit.module("UXReadMore", {
+    setup: function () {
+        $.uxreadmore.defaults.debug = true;
+        $(targetId).empty();
+    },
+    teardown: function () {
+
+    }
+});
+
+test("Default Options", function () {
+    $.uxreadmore.defaults.debug = false;
+    equal($.uxreadmore.defaults.debug, false);
+    equal($.uxreadmore.defaults.ellipsisType, 'absolute');
+    equal($.uxreadmore.defaults.ellipsisPoint, 200);
+    equal($.uxreadmore.defaults.ellipsisSymbol, '...');
+})
+
+test("Absolute - Given text shorter than 200 then nothing should happend", function() {
+    var textDiv = $('<div></div>');
+    textDiv.append(generateText(100));
+    $(targetId).append(textDiv);
+    $(textDiv).uxreadmore();
+
+    var targetTextDiv = $(targetId).find('div');
+    equal($(targetTextDiv).data('cutOff'), "noCut", "CutOff value was not set!")
+    equal($(targetId).find('.hiddenText').length, 0, "Read More was activated!");
+})
+
+test("Absolute - Given text greater than 200 then read more should be present", function() {
+    var mainLength = $.uxreadmore.defaults.ellipsisPoint;
+
+    var textDiv = $('<div></div>');
+    textDiv.append(generateText(250));
+    $(targetId).append(textDiv);
+    $(textDiv).uxreadmore();
+
+    var targetTextDiv = $(targetId).find('div');
+    equal($(targetTextDiv).data('cutOff'), mainLength, "CutOff value was not set!")
+    equal($(targetTextDiv).text().substring(mainLength - 3, mainLength), '...', "Ellipsis not found!"); 
+    equal($(targetId).find('.hiddenText').length, 1, "Overflow text was not found!");
+})
+
+function generateText(length) {
+    return lorem.substring(0, length);
+}
